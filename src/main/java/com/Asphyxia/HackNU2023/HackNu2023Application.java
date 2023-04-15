880360eab166a3f53f800e0fed7a8dd2324899e8
abilmansur <abilmuzhubayev@gmail.com>
2023-04-15 11:50:56 +0600

    Initial commit

---
+++ src/main/java/com/Asphyxia/HackNU2023/HackNu2023Application.java
@@ -0,0 +1,13 @@
+package com.Asphyxia.HackNU2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackNu2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackNu2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Asphyxia/HackNU2023/HackNu2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Asphyxia.HackNU2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackNu2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
