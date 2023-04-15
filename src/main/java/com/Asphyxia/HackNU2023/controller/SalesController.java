1e2bad7fe8f1e19c3912e681259b22f0849021ad
abilmansur <abilmuzhubayev@gmail.com>
2023-04-15 12:08:36 +0600

    Added controller beans and some configurations.

---
+++ src/main/java/com/Asphyxia/HackNU2023/controller/SalesController.java
@@ -0,0 +1,9 @@
+package com.Asphyxia.HackNU2023.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sales")
+public class SalesController {
+}
---
+++ src/main/java/com/Asphyxia/HackNU2023/controller/SuppliesController.java
@@ -0,0 +1,9 @@
+package com.Asphyxia.HackNU2023.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/supplies")
+public class SuppliesController {
+}
